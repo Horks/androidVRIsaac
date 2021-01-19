677fb98f6055ce9b512153bc2f09b29e627af8b2
Horks <ysr.horks@gmail.com>
2021-01-19 08:38:57 -0300

    Primeiro commit

---
+++ Assets/EndOfLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EndOfLevel : MonoBehaviour
+{
+   
+    public void endOfGame()
+    {
+        //Debug.Log("HIT");
+        //Application.Quit();
+        SceneManager.LoadScene("fps");
+
+    }
+}
